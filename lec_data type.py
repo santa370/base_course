e3574268e90edffc398439b2279ebbdf30f1c24d
santa370 <sahsabrosas@gmail.com>
2025-10-18 13:33:37 +0000

    сделал 1 лекцию

---
+++ lec_data type.py
@@ -0,0 +1,20 @@
+type
+
+type(3)
+print(type(3))
+
+print(type(3.4))
+
+print(type( "молодец"))
+
+a = "Molodec"
+print(type(a))
+
+a = ["Molodec", 1, "круто", 6, 8]
+print(type(a))
+
+print(type(True))
+
+print(type(False))
+
+print(type(None))
