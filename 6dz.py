6375641c68b61136d5cfc8a43f1c797a202f7f31
santa370 <sahsabrosas@gmail.com>
2025-10-25 20:49:41 +0000

    Тип да

---
+++ 6dz.py
@@ -0,0 +1,13 @@
+information = []
+
+a =float(input("Введите возраст: "))
+z =input("Введите свой пол: ")
+x =input("Введите свое имя: ")
+c =input("Введите совй город: ")
+v =float(input("Введите в каком вы классе: "))
+
+information.append(a)
+information.append(z)
+information.append(x)
+information.append(c)
+information.append(v)
---
+++ less_math_oper.py
@@ -0,0 +1,13 @@
+print(3+4)
+
+print(3-2)
+
+print(3*2)
+
+print(3**4)
+
+print(3/2)
+
+print(4//3)
+
+print(4%3)
