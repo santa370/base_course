a327b48af323b21dbcc19ee95ad61cb7d74a2b2e
santa370 <sahsabrosas@gmail.com>
2025-10-25 13:00:33 +0000

    Сделелал дз

---
+++ lec_homework6.py
@@ -0,0 +1,22 @@
+information =[]
+
+a =float(input("Введите свой возраст: "))
+
+x =float(input("Введите свой пол: "))
+
+c =float(input("Введите свое имя: "))
+
+v =float(input("Введите свой город: "))
+
+b =float(input("Введите в каком вы классе: "))
+
+information.append(a)
+information.append(x)
+information.append(c)
+information.append(v)
+information.append(b)
+
+
+
+
+
