0f1f04a037970b58a089de2f8c6ed20c3a253e2e
santa370 <sahsabrosas@gmail.com>
2026-01-22 19:40:35 +0000

    dz 2

--- dz1.py
+++ dz1.py
@@ -10,3 +10,16 @@ y = R * (1 - np.cos(t))
 plt.figure(figsize=(10, 4))
 plt.plot(x, y, color='blue')
 plt.savefig('DZ1.png')
+
+plt.close()
+
+
+R = 5
+t = np.linspace(0, 2 * np.pi, 1000)
+
+x = R * np.cos(t)**3
+y = R * np.sin(t)**3
+
+plt.figure(figsize=(6, 6))
+plt.plot(x, y, color='red')
+plt.savefig('DZ1,1.png')
---
+++ dz2.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+
+a = 0.5  
+phi = np.linspace(0, 2 * np.pi, 200)
+
+fig, ax = plt.subplots()
+ax.set_aspect('equal')
